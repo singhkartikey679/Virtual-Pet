f1fcc5ebce078b84fd981fc945b1d6fb558879ee
Kartikey Singh <71145425+singhkartikey679@users.noreply.github.com>
2020-12-23 10:43:30 +0530

    Add files via upload

---
+++ Food.js
@@ -0,0 +1,46 @@
+class Food {
+
+    constructor() {
+        this.image = loadImage("/images/Milk.png");
+    }
+
+    //For Reading value of Firebase database index 'Food'
+    getFoodStock(data) {
+        foodStock = data.val();
+    }
+
+
+    updateFoodStock(value) {
+        if (foodStock > 0) {
+            this.min = minute();
+            if (this.min < 10) {
+                this.min = "0" + this.min;
+            }
+            database.ref('/').update({
+                Food: value,
+                feedTimeHour: hour(),
+                feedTimeMin: this.min,
+                date: day(),
+                month: month(),
+                year: year()
+            });
+        }
+    }
+
+    display() {
+        var x = 70,
+            y = 100;
+
+        imageMode(CENTER);
+        if (foodStock != 0) {
+            for (var i = 0; i < foodStock; i++) {
+                if (i % 10 == 0) {
+                    x = 70;
+                    y += 100;
+                }
+                image(this.image, x, y, 70, 70);
+                x += 35;
+            }
+        }
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,132 @@
+//Creating variable for database
+var database;
+
+var dogImg, dog, happyDog, dogHowlImg, foodStockRef, foodStock;
+
+//Creating variables for button
+var feed, addFood;
+
+//Creating variables to store time
+var fedTimeHour, lastFedHour, fedTimeMin, lastFedMin;
+var date1, month1, year1, date2, month2, year2;
+
+//For 'Food' class
+var foodObj;
+
+var gameState = "start";
+
+function preload() {
+  //load images here
+  dogImg = loadImage("/images/dogImg.png");
+  happyDog = loadImage("/images/dogImg1.png");
+  dogHowlImg = loadImage("/images/dogImgHowl.png");
+}
+
+function setup() {
+  createCanvas(800, 500);
+
+  //Creating object of 'Food' class
+  foodObj = new Food();
+
+
+  dog = createSprite(610, 280);
+  dog.addImage(dogImg);
+  dog.scale = 0.4;
+
+  //Connecting our 'database' variable with Firebase database
+  database = firebase.database();
+
+  //Connecting 'foodStockRef' variable with the Firebase database index 'Food'
+  foodStockRef = database.ref('Food');
+
+  //Reading value of Firebase database index 'Food'
+  foodStockRef.on("value", foodObj.getFoodStock);
+
+  //Creating buttons
+  feed = createButton("Feed the Dog");
+  feed.position(800, 95);
+  feed.mousePressed(feedDog);
+
+  addFood = createButton("Add Food");
+  addFood.position(900, 95);
+  addFood.mousePressed(addFoods);
+
+
+  //Connecting 'fedTime' variable with the Firebase database index 'feedTime'
+  fedTimeHour = database.ref('feedTimeHour');
+  fedTimeMin = database.ref('feedTimeMin');
+  date1 = database.ref('date');
+  month1 = database.ref('month');
+  year1 = database.ref('year');
+
+  //Reading value of Firebase database index 'feedTime'
+  fedTimeHour.on("value", function (data) {
+    lastFedHour = data.val();
+  });
+  fedTimeMin.on("value", function (data) {
+    lastFedMin = data.val();
+  });
+  date1.on("value", function (data) {
+    date2 = data.val();
+  });
+  month1.on("value", function (data) {
+    month2 = data.val();
+  });
+  year1.on("value", function (data) {
+    year2 = data.val();
+  });
+
+}
+
+
+function draw() {
+  background(46, 139, 87);
+
+
+  fill(255, 255, 254);
+  textSize(40);
+  if (lastFedHour >= 12) {
+    text("Last Fed " + lastFedHour % 12 + ":" + lastFedMin + " PM", 80, 70);
+  } else if (lastFedHour == 0) {
+    text("Last Fed 12" + ":" + lastFedMin + " AM", 80, 70);
+  } else {
+    text("Last Fed " + lastFedHour + ":" + lastFedMin + " AM", 80, 70);
+  }
+  text("Date : " + date2 + "-" + month2 + "-" + year2, 80, 130);
+
+  drawSprites();
+  foodObj.display();
+
+  if (gameState == "stop") {
+
+    //Extra emotions of dog
+    if (frameCount % 350 == 0) {
+      dog.addImage(dogHowlImg);
+    }
+
+    //For delaying time after feeding 1 bottle of milk and setting how much of time to be delayed
+    if (frameCount % 425 == 0) {
+      gameState = "start";
+      dog.addImage(dogImg);
+    }
+  }
+
+}
+
+function feedDog() {
+  if (gameState == "start") {
+    dog.addImage(happyDog);
+    foodObj.updateFoodStock(foodStock - 1);
+    gameState = "stop";
+  }
+}
+
+function addFoods() {
+  if (foodStock <= 25)
+    foodStock += 5;
+  else if (foodStock > 25)
+    foodStock += (30 - foodStock);
+  database.ref('/').update({
+    Food: foodStock
+  });
+}
